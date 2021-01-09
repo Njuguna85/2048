0c4b667e68014cca1ca4d29f3afc8eed85319006
Njuguna85 <dennix.njuguna85@gmail.com>
2021-01-09 23:50:32 +0300

    add keystrokes and respective functs

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const resultDisplay = document.getElementById('result')
     const width = 4;
     let squares = [];
+    let score = 0;
 
     // create a playing board
     function createBoard() {
@@ -28,6 +29,7 @@ document.addEventListener('DOMContentLoaded', () => {
         let randomNumber = Math.floor(Math.random() * squares.length)
         if (squares[randomNumber].innerHTML == 0) {
             squares[randomNumber].innerHTML = 2
+            checkForGameOver();
         } else generateNum()
     }
 
@@ -41,15 +43,199 @@ document.addEventListener('DOMContentLoaded', () => {
                 let totalTwo = squares[i + 1].innerHTML;
                 let totalThree = squares[i + 2].innerHTML;
                 let totalFour = squares[i + 3].innerHTML;
+                // create an array with the integer values of the cells
                 let row = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)]
-
-                console.log(row);
+                
+                // get the values of a row
                 let filteredRow = row.filter(num => num)
-                console.log(filteredRow);
+                // get the number of cells with missing values
+                let missing = 4 - filteredRow.length
+                // create an array with the length of missing values and 
+                // fill them with zeros
+                let zeros = Array(missing).fill(0)
+
+                let newRow = zeros.concat(filteredRow)
+
+                squares[i].innerHTML = newRow[0]
+                squares[i + 1].innerHTML = newRow[1];
+                squares[i + 2].innerHTML = newRow[2];
+                squares[i + 3].innerHTML = newRow[3];
             }
         }
     }
 
 
-    moveRight()
+    // swipe left
+    function moveLeft() {
+        for (let i = 0; i < width * width; i++) {
+            // i is a cell in the grid
+            // get the contents of the cells as a whole row
+            if (i % 4 === 0) {
+                let totalOne = squares[i].innerHTML;
+                let totalTwo = squares[i + 1].innerHTML;
+                let totalThree = squares[i + 2].innerHTML;
+                let totalFour = squares[i + 3].innerHTML;
+                // create an array with the integer values of the cells
+                let row = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)]
+
+                // get the values of a row
+                let filteredRow = row.filter(num => num)
+                // get the number of cells with missing values
+                let missing = 4 - filteredRow.length
+                // create an array with the length of missing values and 
+                // fill them with zeros
+                let zeros = Array(missing).fill(0)
+                let newRow = filteredRow.concat(zeros)
+
+                squares[i].innerHTML = newRow[0]
+                squares[i + 1].innerHTML = newRow[1];
+                squares[i + 2].innerHTML = newRow[2];
+                squares[i + 3].innerHTML = newRow[3];
+            }
+        }
+    }
+
+    // swipe down
+    function moveDown() {
+        for (let i = 0; i < 4; i++) {
+            let totalOne = squares[i].innerHTML;
+            let totalTwo = squares[i + width].innerHTML;
+            let totalThree = squares[i + (width * 2)].innerHTML;
+            let totalFour = squares[i + (width * 3)].innerHTML;
+
+            let column = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)]
+
+            let filteredColumn = column.filter(num => num)
+            let missing = 4 - filteredColumn.length
+            let zeros = Array(missing).fill(0)
+            let newColumn = zeros.concat(filteredColumn)
+
+            squares[i].innerHTML = newColumn[0]
+            squares[i + width].innerHTML = newColumn[1]
+            squares[i + (width * 2)].innerHTML = newColumn[2]
+            squares[i + (width * 3)].innerHTML = newColumn[3]
+
+        }
+    }
+
+
+    function moveUp() {
+        for (let i = 0; i < 4; i++) {
+            let totalOne = squares[i].innerHTML;
+            let totalTwo = squares[i + width].innerHTML;
+            let totalThree = squares[i + (width * 2)].innerHTML;
+            let totalFour = squares[i + (width * 3)].innerHTML;
+
+            let column = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)]
+
+            let filteredColumn = column.filter(num => num)
+            let missing = 4 - filteredColumn.length
+            let zeros = Array(missing).fill(0)
+            let newColumn = filteredColumn.concat(zeros)
+
+            squares[i].innerHTML = newColumn[0]
+            squares[i + width].innerHTML = newColumn[1]
+            squares[i + (width * 2)].innerHTML = newColumn[2]
+            squares[i + (width * 3)].innerHTML = newColumn[3]
+
+        }
+    }
+
+    function combineRow() {
+        for (let i = 0; i < 15; i++) {
+            if (squares[i].innerHTML === squares[i + 1].innerHTML) {
+                let combinedTotal = parseInt(squares[i].innerHTML) + parseInt(squares[i + 1].innerHTML)
+                squares[i].innerHTML = combinedTotal
+                squares[i + 1].innerHTML = 0
+                score += combinedTotal;
+                scoreDisplay.innerHTML = score;
+            }
+        }
+        checkForWin()
+    }
+
+    function combineColumn() {
+        for (let i = 0; i < 12; i++) {
+            if (squares[i].innerHTML === squares[i + width].innerHTML) {
+                let combinedTotal = parseInt(squares[i].innerHTML) + parseInt(squares[i + width].innerHTML)
+                squares[i].innerHTML = combinedTotal
+                squares[i + width].innerHTML = 0;
+                score += combinedTotal;
+                scoreDisplay.innerHTML = score;
+            }
+        }
+        checkForWin()
+    }
+
+
+    // assign keystrokes
+    function control(e) {
+        // right arrow
+        if (e.keyCode === 39) {
+            keyRight()
+        } else if (e.keyCode === 37) {
+            keyLeft()
+        } else if (e.keyCode === 38) {
+            keyUp()
+        } else if (e.keyCode == 40) {
+            keyDown()
+        }
+
+    }
+    document.addEventListener('keyup', control)
+
+    function keyRight() {
+        moveRight()
+        combineRow()
+        moveRight()
+        generateNum()
+    }
+
+    function keyLeft() {
+        moveLeft()
+        combineRow()
+        moveLeft()
+        generateNum()
+    }
+
+    function keyDown() {
+        moveDown()
+        combineColumn()
+        moveDown()
+        generateNum()
+    }
+
+    function keyUp() {
+        moveUp()
+        combineColumn()
+        moveUp()
+        generateNum()
+    }
+
+
+    // check for win
+    function checkForWin() {
+        for (let i = 0; i < squares.length; i++) {
+            if (squares[i].innerHTML == 2048) {
+                resultDisplay.innerHTML = 'You win';
+                document.removeEventListener('keyup', control)
+            }
+        }
+    }
+
+    // chekck if there are no zeros on the board to loose
+    function checkForGameOver() {
+        let zeros = 0;
+        for (let i = 0; i < squares.length; i++) {
+            if (squares[i].innerHTML == 0) {
+                zeros++
+            }
+
+        }
+        if (zeros === 0) {
+            resultDisplay.innerHTML = 'Game Over';
+            document.removeEventListener('keyup', control)
+
+        }
+    }
 });
